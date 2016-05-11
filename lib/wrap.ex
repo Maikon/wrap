c9d676504c63d9f6a0995de2639837cdde318d3e
Makis Otman <makisotman@gmail.com>
2016-05-11 11:46:59 +0100

    Stage 3

--- lib/wrap.ex
+++ lib/wrap.ex
@@ -1,25 +1,34 @@
 defmodule Wrap do
+  @break "\n"
+  @space " "
 
   def wrap(text, col_number) do
-    list_of_words = String.split(text, " ")
+    list_of_words = String.split(text, @space)
     do_wrap(col_number, "", list_of_words)
   end
 
-  defp do_wrap(_word_boundary, result, []), do: String.strip(result) <> "\n"
+  defp do_wrap(_word_boundary, result, []), do: String.strip(result) <> @break
   defp do_wrap(word_boundary, result, [word | rest]) do
-
-    lines = String.split(result, "\n")
-    current_line_size = List.last(lines) |> String.length
-    attempted_line = current_line_size + String.length(word)
+    attempted_line = attempt_line(result, word)
 
     cond do
       attempted_line > word_boundary ->
-        do_wrap(word_boundary, String.replace_trailing(result, " ", "\n") <> word <> " ", rest)
+        do_wrap(word_boundary, replace_space_with_break(result) <> word <> @space, rest)
       attempted_line == word_boundary ->
-        do_wrap(word_boundary, result <> word <> "\n", rest)
+        do_wrap(word_boundary, result <> word <> @break, rest)
       attempted_line < word_boundary ->
-        do_wrap(word_boundary, result <> word <> " ", rest)
+        do_wrap(word_boundary, result <> word <> @space, rest)
     end
   end
+
+  defp attempt_line(result, word) do
+    current_line_size = String.split(result, @break)
+                        |> List.last
+                        |> String.length
+
+    current_line_size + String.length(word)
+  end
+
+  defp replace_space_with_break(result), do: String.replace_trailing(result, @space, @break)
 end
 
